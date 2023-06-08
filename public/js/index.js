13ccd328e55568f3f73ba90fabf76518efd0c51a
DjibySec4 <djibysec4@gmail.com>
2023-06-08 12:27:15 +0000

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+// slection multiple
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
+});
+
+// single selection
+$(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
